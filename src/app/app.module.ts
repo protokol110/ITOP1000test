8d6e4726927b1bb6621c5ec4fab6b988b2655223
protokol110 <asinakevic@gmail.com>
2022-04-12 19:41:39 +0300

    header without design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {ConverterComponent} from "./converter/converter.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ConverterComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    FormsModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
